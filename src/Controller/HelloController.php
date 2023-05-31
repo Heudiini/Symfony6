66798d1ba7b10c43296a2741ef5ed61371f950dd
Heudiini <77001762+Heudiini@users.noreply.github.com>
2023-05-31 20:23:37 +0100

    setting twig templates

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,23 +2,36 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HelloController
+class HelloController extends AbstractController
+
 {
   private array $messages = [
     "Hello", "Hi", "Bye!"
   ];
-
-  #[Route('/', name: 'app_index')]
-public function index(): Response
+   #[Route('/{limit?3}', name: 'app_index')]
+  public function index(int $limit): Response
 {
-    return new Response(implode(',', $this->messages));
-  }
+    return $this->render(
+        'hello/index.html.twig',
+        [
+            'message' => implode(',', array_slice($this->messages, 0, $limit))
+        ]
+    );
+}
 
-  #[Route('/messages/{id}', name: 'app_show_one')]
-  public function showOne($id): Response
+  #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+  public function showOne(int $id): Response
   {
-    return new Response($this->messages[$id]);
+      return $this->render(
+        // takes two arguments inside '', : name of the template file, path
+      'hello/show_one.html.twig',
+      [
+        'message' => $this->messages[$id]
+      ]
+    );
+    // return new Response($this->messages[$id]);
   }
 }
 /*
