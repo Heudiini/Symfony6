1fb60284da14c619d61b18e60ebebf8bc354df68
Heudiini <77001762+Heudiini@users.noreply.github.com>
2023-06-06 16:32:26 +0100

    works without the pack that is no longer supported

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -12,23 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(MicroPostRepository $posts, EntityManagerInterface $entityManager): Response
+    public function index(MicroPostRepository $posts): Response
     {
-        // Uncomment the code below if you want to remove a MicroPost by ID
-        /*
-        $microPost = $posts->find(5);
-        if ($microPost) {
-            $entityManager->remove($microPost);
-            $entityManager->flush();
-        }
-        */
-
         // Get all MicroPosts
         $allPosts = $posts->findAll();
         dd($allPosts);
 
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $allPosts,
         ]);
     }
 
@@ -39,6 +30,8 @@ class MicroPostController extends AbstractController
         $post = $posts->find($id);
         dd($post);
 
-        return new Response();
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
+        ]);
     }
 }
